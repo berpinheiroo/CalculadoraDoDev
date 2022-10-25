4844ecdb343a0fc09d9af7d32be84335eeb63fdd
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-24 22:25:09 -0300

    Adicionando calculo de soma

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var num1 = parseInt(prompt("Insira um numero"))
 var num2 = parseInt(prompt("Insira outro numero"))
+
+SomaNum()
+    var soma
+    function SomaNum() {
+        soma = num1 + num2
+        return soma
+    }
+    console.log("O resultado da soma e:", soma)
