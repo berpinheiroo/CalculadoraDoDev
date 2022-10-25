8a402041d031ade91948c622eafb77c68a1318aa
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-10-24 22:27:00 -0300

    Adicionando calculo de subtracao

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var num1 = parseInt(prompt("Insira um numero"))
 var num2 = parseInt(prompt("Insira outro numero"))
 
-SomaNum()
+    SomaNum()
     var soma
     function SomaNum() {
         soma = num1 + num2
         return soma
     }
     console.log("O resultado da soma e:", soma)
+
+    SubNum()
+    var sub
+    function SubNum() {
+        sub = num1 - num2
+        return sub
+    }
+    console.log("O resultado da subtracao e: ", sub)
